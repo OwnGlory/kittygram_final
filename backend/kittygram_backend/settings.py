7697062b639785a0d837cfe89976f8c3d57ac4d4
Vlad.S <inf.vlad.s@bk.ru>
2023-12-20 15:12:52 +0300

    Change settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'secret_key')
 
-DEBUG = os.getenv('DEBUG', 'debug')
+DEBUG = os.getenv('DEBUG',)
 
 ALLOWED_HOSTS = []
 
